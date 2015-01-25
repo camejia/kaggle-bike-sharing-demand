75d5973f81ed1d9f583e18a3dfeb4954dcb61754
camejia <camejia@alum.mit.edu>
2015-01-25 07:44:24 -0800

    I started an R file

---
+++ forecast_bike_sharing_demand.R
@@ -0,0 +1,6 @@
+train = read.csv(file.path('C:', 'Users', 'Chris', 'Documents', 'GitHub',
+                           'kaggle-bike-sharing-demand', 'train2.csv'))
+test = read.csv(file.path('C:', 'Users', 'Chris', 'Documents', 'GitHub',
+                           'kaggle-bike-sharing-demand', 'test.csv'))
+tra
+C:\Users\Chris\Documents\GitHub\kaggle-bike-sharing-demand\test.csv
